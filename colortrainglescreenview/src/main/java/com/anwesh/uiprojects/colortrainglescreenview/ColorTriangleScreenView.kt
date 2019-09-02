3e55553693b5e8a53969e4c9554aa00136b16f07
Anwesh43 <anweshthecool0@gmail.com>
2019-09-02 16:59:15 +0530

    adding constants and libraries

--- colortrainglescreenview/src/main/java/com/anwesh/uiprojects/colortrainglescreenview/ColorTriangleScreenView.kt
+++ colortrainglescreenview/src/main/java/com/anwesh/uiprojects/colortrainglescreenview/ColorTriangleScreenView.kt
@@ -3,3 +3,17 @@ package com.anwesh.uiprojects.colortrainglescreenview
 /**
  * Created by anweshmishra on 02/09/19.
  */
+
+import android.content.Context
+import android.app.Activity
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<String> = arrayOf("#00BFA5", "#9C27B0", "#64DD17", "#f44336", "#0D47A1")
+val scGap : Float = 0.05f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val triSizeFactor : Float = 3f

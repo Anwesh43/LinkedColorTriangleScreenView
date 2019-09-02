0b6988c4fec94141d3a58ca751d2ff1ec8ad4e42
Anwesh43 <anweshthecool0@gmail.com>
2019-09-02 17:20:26 +0530

    adding ColorTriangleScreenView class

--- colortrainglescreenview/src/main/java/com/anwesh/uiprojects/colortrainglescreenview/ColorTriangleScreenView.kt
+++ colortrainglescreenview/src/main/java/com/anwesh/uiprojects/colortrainglescreenview/ColorTriangleScreenView.kt
@@ -49,3 +49,21 @@ fun Canvas.drawTriangleScreen(i : Int, scale : Float, sc : Float, paint : Paint)
     restore()
     restore()
 }
+
+class ColorTriangleScreenView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}

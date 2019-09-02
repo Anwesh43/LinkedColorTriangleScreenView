9bf356ddfb7e02dfb790659276338e0bc8945b9e
Anwesh43 <anweshthecool0@gmail.com>
2019-09-02 18:01:20 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedcolortrianglescreenview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcolortrianglescreenview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedcolortrianglescreenview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.colortrainglescreenview.ColorTriangleScreenView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ColorTriangleScreenView.create(this)
     }
 }
